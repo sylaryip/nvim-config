1c1b051dca20b378a57fedb22e39af75be9f4021
yezhihao <yezhihao@topgamesinc.com>
2023-01-28 02:34:55 +0800

    fix bug

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -90,17 +90,6 @@ return require('packer').startup(function(use)
     requires = { {"nvim-tree/nvim-web-devicons"} }
   })
 
-  use ({
-    'ray-x/lsp_signature.nvim',
-    config = function()
-      require('lsp_signature').setup({
-        bind = true,
-        handler_opts = {
-          border = 'rounded'
-        }
-      })
-    end
-  })
 
   if packer_bootstrap then
     require('packer').sync()
